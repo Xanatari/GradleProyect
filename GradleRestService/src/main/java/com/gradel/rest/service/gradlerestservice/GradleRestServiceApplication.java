0ad100786614fd80a0e0cf85d4364f6ca7a8c1de
Diego Orlando Gonzalez Cruz <dogc.1994@gmail.com>
2021-06-04 22:28:08 -0500

    Firts Commit

---
+++ GradleRestService/src/main/java/com/gradel/rest/service/gradlerestservice/GradleRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gradel.rest.service.gradlerestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleRestServiceApplication.class, args);
+	}
+
+}
---
+++ GradleRestService/src/test/java/com/gradel/rest/service/gradlerestservice/GradleRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gradel.rest.service.gradlerestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
