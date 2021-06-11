de49bac86fb86e653bc6d7913be008dd0eba1514
Diego Orlando Gonzalez Cruz <dogc.1994@gmail.com>
2021-06-10 22:40:00 -0500

    Update GenericResponse.java

--- GradleRestService/src/main/java/com/gradel/rest/service/gradlerestservice/dtos/Response/Generics/GenericResponse.java
+++ GradleRestService/src/main/java/com/gradel/rest/service/gradlerestservice/dtos/Response/Generics/GenericResponse.java
@@ -1,4 +1,17 @@
 package com.gradel.rest.service.gradlerestservice.dtos.Response.Generics;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Setter
+@Getter
 public class GenericResponse {
+
+    private String code;
+    private String status;
+    private Object msg;
+    private String exception;
+
 }
