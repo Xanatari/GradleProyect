83d77eab22c17c32c36941d1ad31112df7d0ea45
Diego Orlando Gonzalez Cruz <dogc.1994@gmail.com>
2021-06-10 20:03:54 -0500

    Create ConnectorBussinessConstants.java

---
+++ GradleRestService/src/main/java/com/gradel/rest/service/gradlerestservice/constans/ConnectorBussinessConstants.java
@@ -0,0 +1,8 @@
+package com.gradel.rest.service.gradlerestservice.constans;
+
+public class ConnectorBussinessConstants {
+
+    public static final String HEADER_TENANT_ID = "X-CONTEXT_ID";
+
+    private ConnectorBussinessConstants() {throw  new IllegalStateException("Utility class");}
+}
