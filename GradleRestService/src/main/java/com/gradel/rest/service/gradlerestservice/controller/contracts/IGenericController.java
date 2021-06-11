f4cce93102297b443a1b9b563e17e4dabe5b1734
Diego Orlando Gonzalez Cruz <dogc.1994@gmail.com>
2021-06-10 21:59:28 -0500

    Update IGenericController.java

--- GradleRestService/src/main/java/com/gradel/rest/service/gradlerestservice/controller/contracts/IGenericController.java
+++ GradleRestService/src/main/java/com/gradel/rest/service/gradlerestservice/controller/contracts/IGenericController.java
@@ -1,9 +1,10 @@
 package com.gradel.rest.service.gradlerestservice.controller.contracts;
 
+import com.gradel.rest.service.gradlerestservice.dtos.Request.Generics.GenericRequest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 
 public interface IGenericController {
 
-    ResponseEntity<Object> registerUser(HttpHeaders httpHeaders);
+    ResponseEntity<Object> registerUser(HttpHeaders httpHeaders, GenericRequest genericRequestCustomer);
 }
